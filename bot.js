b87897116129a94c8d39e8258f85161bacd53ee2
Gajarbotol <123944033+Gajarbotol@users.noreply.github.com>
2024-06-02 19:28:21 +0600

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+
+const token = '6531689450:AAFNTGx4bafhOll-pS2ySRRs7eAsILA9iUw'; // replace 'YOUR_BOT_TOKEN' with your actual bot token
+const bot = new TelegramBot(token, {polling: true});
+
+function sendJoke(chatId) {
+  axios.get('https://official-joke-api.appspot.com/random_joke')
+    .then(response => {
+      const joke = `${response.data.setup} - ${response.data.punchline}`;
+      bot.sendMessage(chatId, joke);
+    })
+    .catch(error => {
+      console.log(error);
+      bot.sendMessage(chatId, 'Sorry, I couldn\'t fetch a joke for you.');
+    });
+}
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  sendJoke(chatId);
+  setInterval(sendJoke, 5 * 60 * 1000, chatId); // Send a joke every 5 minutes
+});
