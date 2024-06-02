4733bf8d891fe09099c609913ab0bca856403a4f
Gajarbotol <123944033+Gajarbotol@users.noreply.github.com>
2024-06-02 19:43:05 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,37 +1,28 @@
 const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios');
-
-// Replace 'YOUR_BOT_TOKEN' with your actual bot token
-const token = '6531689450:AAFNTGx4bafhOll-pS2ySRRs7eAsILA9iUw';
-const bot = new TelegramBot(token, {polling: true});
-
-function sendJoke(chatId) {
-  axios.get('https://official-joke-api.appspot.com/random_joke')
-    .then(response => {
-      const joke = `${response.data.setup} - ${response.data.punchline}`;
-      bot.sendMessage(chatId, joke);
-    })
-    .catch(error => {
-      console.log(error);
-      bot.sendMessage(chatId, 'Sorry, I couldn\'t fetch a joke for you.');
-    });
-}
-
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  sendJoke(chatId);
-  setInterval(sendJoke, 5 * 60 * 1000, chatId); // Send a joke every 5 minutes
-});
-
-// Render web services listen on port 10000 by default
-const port = 10000;
 const express = require('express');
 const app = express();
 
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '6531689450:AAFNTGx4bafhOll-pS2ySRRs7eAsILA9iUw';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const firstName = msg.from.first_name;
+  const lastName = msg.from.last_name;
+  const languageCode = msg.from.language_code;
+
+  bot.sendMessage(chatId, `Hello, ${firstName} ${lastName}! Your chat ID is ${chatId} and your language is ${languageCode}.`);
+});
+
+// Start Express server
 app.get('/', (req, res) => {
   res.send('Telegram bot is running...');
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
